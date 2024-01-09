12bac105c45d9520ffb756d5391071423de3f4b6
Lyanis Souidi <code@lyanis.dev>
2024-01-09 21:13:56 +0100

    Ajout du remplissage des cellules dans le tableau

--- src/Worksheet.java
+++ src/Worksheet.java
@@ -4,7 +4,13 @@ public class Worksheet {
     private Map<String, Cell> cellsMap;
 
     public Worksheet() {
-        this.cellsMap = new HashMap<>();
+        this.cellsMap = new HashMap<String, Cell>();
+        for (int i = 1; i < 10; i++) {
+            for (int j = 1; j < 10; j++) {
+                String location = String.valueOf((char)(i + 64)) + j;
+                this.cellsMap.put(location, new Cell(this, location));
+            }
+        }
     }
 
     public Cell get(String cellName) {
